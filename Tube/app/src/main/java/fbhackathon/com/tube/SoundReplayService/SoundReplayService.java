fd0d56cb3993ac4add2786c2dce49a3cb3c7121b
Johann Thor Kristthorsson <johannth@Johanns-MacBook-Pro.local>
2016-03-12 18:24:40 +0000

    Starting the TextToSpeech

--- Tube/app/src/main/java/fbhackathon/com/tube/SoundReplayService/SoundReplayService.java
+++ Tube/app/src/main/java/fbhackathon/com/tube/SoundReplayService/SoundReplayService.java
@@ -7,8 +7,11 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.IBinder;
+import android.speech.tts.TextToSpeech;
+
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import fbhackathon.com.tube.R;
 
@@ -21,9 +24,14 @@ public class SoundReplayService extends IntentService {
     @Override
     protected void onHandleIntent(Intent workIntent) {
         // Gets data from the incoming Intent
-        Uri data = workIntent.getData();
+        //Uri data = workIntent.getData();
         // Do work here, based on the contents of dataString
 
+        TextToSpeech tts = new TextToSpeech(this,new TTSListener());
+        String dataString = workIntent.getDataString();
+        String utteranceId=this.hashCode() + "";
+        tts.speak(dataString, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
+        /*
         MediaPlayer mp = new MediaPlayer();
         try {
             mp.setDataSource("/sdcard/tubeapp/shotgun.mp3");
@@ -31,7 +39,8 @@ public class SoundReplayService extends IntentService {
         }catch(IOException e){
             e.printStackTrace();
         }
+        */
         //mp.setLooping(true);
-        mp.start();
+        //mp.start();
     }
 }
---
+++ Tube/app/src/main/java/fbhackathon/com/tube/SoundReplayService/TTSListener.java
@@ -0,0 +1,13 @@
+package fbhackathon.com.tube.SoundReplayService;
+
+import android.speech.tts.TextToSpeech;
+
+/**
+ * Created by johannth on 12/03/16.
+ */
+public class TTSListener implements TextToSpeech.OnInitListener {
+    @Override
+    public void onInit(int status) {
+
+    }
+}
